c85ff1773b0a4e228762f6e7e51e0368abbafe88
ahviplc <ahlc@sina.cn>
2021-01-28 10:50:32 +0800

    nodejs:update Learn_Promise.js.

--- nodejsKu/Learn_Promise.js
+++ nodejsKu/Learn_Promise.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 function readFile(path, isSetError) {
     let promise = new Promise((resolve, reject) => {
@@ -37,5 +38,24 @@ async function run() {
     console.log(res)
 }
 
+// 方法3 优化文件获取 使用绝对路径
+async function run2() {
+    let res = await readFile(path.join(__dirname, "./data/data.json"))
+    console.log(res)
+}
+
 // 执行
-run()
+run2()
+
+// console.log('我直接过')
+//
+// console.log(__filename) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku\nodejsKu\Learn_Promise.js
+//
+// console.log(__dirname) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku\nodejsKu (__dirname表示当前文件的目录名)
+// console.log(path.dirname(__filename)) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku\nodejsKu
+//
+// console.log(path.resolve(__dirname, '..')) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku
+// console.log(path.dirname(__dirname)) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku
+//
+// // 这种写法适配各个操作系统
+// console.log(path.join(__dirname, "./data/data.json")) // C:\_developSoftKu\ideaIU-2019.1.3.win\#CodeKu\vscodeku\nodejsKu\data\data.json
